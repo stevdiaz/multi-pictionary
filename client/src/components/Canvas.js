13c7d3c8a1c49a8acf365830ff57c9516c5b0f38
Steven Diaz <stevdiaz@mit.edu>
2021-05-17 18:11:02 -0400

    removing emit draw commands

--- client/src/components/Canvas.js
+++ client/src/components/Canvas.js
@@ -38,11 +38,9 @@ export default class Canvas extends React.Component {
             if (!this.state.isDrawing && this.props.isIndexPoint && !prevProps.isIndexPoint) {
                 this.onStartDraw(transformedHandPos);
                 this.props.onStartDraw();
-                // emitDraw();
             } else if (this.state.isDrawing && this.props.isIndexPoint && prevProps.isIndexPoint && 
                 this.props.handPos !== prevProps.handPos) {
                 this.onMoveFinger(transformedHandPos);
-                // emitDraw();
             } else if (this.state.isDrawing && !this.props.isIndexPoint && prevProps.isIndexPoint) {
                 this.onEndDraw();
                 this.props.onEndDraw();
